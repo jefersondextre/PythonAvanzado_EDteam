7e71c7444ae06c36d4d3cb4704547a85eb4e5227
jadex_desktop_home <jadexb04@gmail.com>
2023-08-30 12:18:01 -0500

    Prueba 7

--- modulo1/ahorcado/main.py
+++ modulo1/ahorcado/main.py
@@ -54,7 +54,6 @@ palabras = """valoracion aprenderpython comida juego python web imposible
            lapicero celular valor revista gratuito disco voleibol
            anillo estrella""".split()
 
-
 def buscarPalabraAleat(listaPalabras):
     # Esta funcion retorna una palabra aleatoria.
     palabraAleatoria = random.randint(0, len(listaPalabras) - 1)
